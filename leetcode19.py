bdb156e56e0d7e055c0560a1639054df0556d3a5
Alice00021 <alisa_barysheva03@mail.ru>
2024-03-22 11:56:36 +0300

    majorityElement

---
+++ leetcode19.py
@@ -0,0 +1,9 @@
+def majorityElement(nums):
+    res, count=0, 0
+    for n in nums:
+        if count==0:
+            res=n
+        count+=(1 if n==res else -1)
+    return res
+
+print(majorityElement([3,2,3]))
